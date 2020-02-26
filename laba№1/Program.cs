35a0d3357dedc6827f661ed224bf16623b44809c
Olesia-Kravchenko193 <yaseloklass@gmail.com>
2020-02-26 13:14:20 +0200

    1

---
+++ laba№1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, Z;
+            Console.WriteLine("enter an integer a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter an integer b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Z = Math.Pow((a - b), 2);
+            Console.WriteLine("Z= " + Z);
+        }
+    }
+}
